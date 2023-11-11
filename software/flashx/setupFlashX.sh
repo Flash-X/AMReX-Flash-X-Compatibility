a0f5136bba849117d7644b87dbed2af93580b504
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-11 11:29:20 -0600

    update flashx branch to staged

--- software/flashx/setupFlashX.sh
+++ software/flashx/setupFlashX.sh
@@ -5,9 +5,9 @@ fi
 
 # Clone a fresh copy for building and testing
 if [[ $GitConnection == "SSH" ]]; then
-	git clone git@github.com:Flash-X/Flash-X --branch main Flash-X && cd Flash-X
+	git clone git@github.com:Flash-X/Flash-X --branch staged Flash-X && cd Flash-X
 elif [[ $GitConnection == "HTTPS" ]]; then
-	git clone https://github.com/Flash-X/Flash-X.git --branch main Flash-X && cd Flash-X
+	git clone https://github.com/Flash-X/Flash-X.git --branch staged Flash-X && cd Flash-X
 else
 	exit 1
 fi
