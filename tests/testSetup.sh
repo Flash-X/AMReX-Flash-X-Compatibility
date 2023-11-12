8145d599edb3c2c91de3da6d1c40b216fe02c60f
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-12 02:15:03 -0600

    update test setup

--- tests/testSetup.sh
+++ tests/testSetup.sh
@@ -8,7 +8,6 @@ ln -sf $SiteHome/Makefile.h $FLASHX_HOME/sites/$TestSiteName/Makefile.h
 flashxtest init -z $FLASHX_HOME \
 	-s $TestSiteName -m $FLASHTEST_MAIN_ARCHIVE \
 	-o $FLASHTEST_OUTPUT_DIR \
-	-vv $FLASHTEST_VIEW_ARCHIVE \
 	-a $FLASHTEST_LOCAL_ARCHIVE -mpi=mpirun -make="make -j"
 
 # Initialiaze Flash-X test suite
