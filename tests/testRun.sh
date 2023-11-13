d4e1606de057caa2004bc356642ca74a96ef47db
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-13 13:32:51 -0600

    fix bug

--- tests/testRun.sh
+++ tests/testRun.sh
@@ -1,9 +1,9 @@
 # cache the value of current working directory
 NodeDir=$(realpath .)
 
-cd $PROJECT_HOME/software/flashx/Flash-X && Flash-X HEAD is at $(git rev-parse --short HEAD)
-cd $PROJECT_HOME/software/amrex/AMReX && AMReX HEAD is at $(git rev-parse --short HEAD)
-cd $PROJECT_HOME/software/bittree/Bittree && Bittree HEAD is at $(git rev-parse --short HEAD)
+cd $PROJECT_HOME/software/flashx/Flash-X && echo Flash-X HEAD is at $(git rev-parse --short HEAD)
+cd $PROJECT_HOME/software/amrex/AMReX && AMReX echo HEAD is at $(git rev-parse --short HEAD)
+cd $PROJECT_HOME/software/bittree/Bittree && echo Bittree HEAD is at $(git rev-parse --short HEAD)
 
 # Run Flash-X test suite
 cd $NodeDir && flashxtest run-suite --archive
