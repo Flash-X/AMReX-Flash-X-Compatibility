f5bc484a2ebaabe04115cc26875cb43aad41d15f
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-13 13:58:10 -0600

    bug fix again

--- tests/testRun.sh
+++ tests/testRun.sh
@@ -2,7 +2,7 @@
 NodeDir=$(realpath .)
 
 cd $PROJECT_HOME/software/flashx/Flash-X && echo Flash-X HEAD is at $(git rev-parse --short HEAD)
-cd $PROJECT_HOME/software/amrex/AMReX && AMReX echo HEAD is at $(git rev-parse --short HEAD)
+cd $PROJECT_HOME/software/amrex/AMReX && echo AMReX HEAD is at $(git rev-parse --short HEAD)
 cd $PROJECT_HOME/software/bittree/Bittree && echo Bittree HEAD is at $(git rev-parse --short HEAD)
 
 # Run Flash-X test suite
